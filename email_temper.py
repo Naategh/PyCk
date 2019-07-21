9cde23f567fe47d59665bed750a5b710911126ea
Naategh <manamtabeshekan@gmail.com>
2019-07-21 19:31:15 +0430

    Updated

--- email_temper.py
+++ email_temper.py
@@ -1,12 +1,12 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # We use this library for "Web Scraping", See References in README.md for more details
 
 url = 'https://tempail.com/en/'
 html = urlopen(url)
-bs = BeautifulSoup(html.read(),'html.parser')
+bs = BeautifulSoup(html.read(), 'html.parser')
 
-for e in bs.find_all('input',{'id':'eposta_adres'}):
+for e in bs.find_all('input', {'id': 'eposta_adres'}):  # Find our temporary email
     if 'value' in e.attrs:
-        print('Your temp email is:\t',e.attrs['value'])
+        print('Your temp email is:\t', e.attrs['value'])
     else:
         print('Couldn\'t get temp mail!')
